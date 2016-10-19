fe91cf895fb848c97115f872d5448dd931f83c01
dburriss <devon@chimplab.co>
2016-10-19 08:57:10 +0200

    Fail silently as default. Allows setting of a boolean to throw errors.

---
+++ test/YesSensu.Tests/SensuTcpClientTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace YesSensu.Tests
+{
+    public class SensuTcpClientTests
+    {
+        //TODO: there are different contraints on Connect and Send across TCP and UDP. This is violation of LSP. Come back and fix this.
+        [Fact]
+        public void Connect_WithUnavailableServerAndThrowOnErrorFalse_DoesNotThrowError()
+        {
+            var hasError = false;
+            var sut = new SensuTcpClient("iamaghost", 0000, false);
+            
+            try
+            {
+                sut.Connect();
+            }
+            catch (Exception)
+            {
+                hasError = true;
+            }
+
+            Assert.False(hasError);
+        }
+
+        [Fact]
+        public void Connect_WithUnavailableServerAndThrowOnErrorTrue_ThrowsError()
+        {
+            var sut = new SensuTcpClient("iamaghost", 0000, true);
+            
+            Assert.Throws<TimeoutException>(() => sut.Connect());
+            
+        }
+    }
+}
---
+++ test/YesSensu.Tests/SensuUdpClientTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+using Xunit;
+using YesSensu.Messages;
+
+namespace YesSensu.Tests
+{
+    public class SensuUdpClientTests
+    {
+        [Fact]
+        public void Send_WithUnavailableServerAndThrowOnErrorFalse_DoesNotThrowError()
+        {
+            var hasError = false;
+            var sut = new SensuUdpClient("iamaghost", 0000, false);
+            sut.Connect();
+
+            try
+            {
+                sut.Send(new Ok("MyApp", "SomeMetric"));
+            }
+            catch (Exception)
+            {
+                hasError = true;
+            }
+
+            Assert.False(hasError);
+        }
+
+        [Fact]
+        public void Send_WithUnavailableServerAndThrowOnErrorTrue_ThrowsError()
+        {
+            var sut = new SensuUdpClient("iamaghost", 0000, true);
+            sut.Connect();
+            Assert.Throws<SocketException>(() => sut.Send(new Ok("MyApp", "SomeMetric")));
+            
+        }
+    }
+}
